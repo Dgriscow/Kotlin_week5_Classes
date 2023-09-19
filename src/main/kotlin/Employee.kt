15c98d5dee357e49a8c893045d196d98cc264a16
dgriscow <dgrischow@students.mchenry.edu>
2023-09-19 17:15:29 -0500

    Initial commit

---
+++ src/main/kotlin/Employee.kt
@@ -0,0 +1,20 @@
+class Contacts(var employeeName:String,
+    var employeeNum:Int,
+    var employeeShift:Int){
+
+
+
+    fun displayEmployeeInfo(){
+        println("sus")
+    }
+
+    
+
+
+
+}
+
+
+
+
+

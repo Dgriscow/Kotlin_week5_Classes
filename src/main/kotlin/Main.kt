1eb544cdb9e79b212e6d061d699e9007de2f5dc9
dgriscow <dgrischow@students.mchenry.edu>
2023-09-21 14:27:26 -0500

    commited and completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
-    var jim = Employee("jimmey", 432423, 3)
+    val saul = Employee("Jimmy Mcgill", 1897, 1)
 
-    jim.displayEmployeeInfo()
+    saul.displayEmployeeInfo()
+
+    val heisen = Employee("Walter white", 7647, 0)
+
+    println()
+
+    heisen.displayEmployeeInfo()
+
+    println()
+
+    val mike = Employee("Mike Ehrmantraut", 1 ,3)
+
+    mike.displayEmployeeInfo()
 }
